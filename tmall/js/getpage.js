e0368700b248f5aeee149f29cacc523311e0c71e
isuye <479333117@qq.com>
2019-03-06 16:51:18 +0800

    轮播图

--- tmall/js/getpage.js
+++ tmall/js/getpage.js
@@ -33,8 +33,10 @@ newpage();
 newBrand();
 //商品分类 女王节
 var li = document.getElementById('nav-title').children
-li[0].addEventListener("mouseover",function(){
-    li[0].style.background = "white";
+li[0].addEventListener("mouseover",function(e){
+    
+    e.target.style.background="white";
+    li[0].children[0].style.background = "white";
     li[0].children[0].style.color = "red";
 
     li[1].style.background = "red";
@@ -46,30 +48,71 @@ li[1].addEventListener("mouseover",function(){
     li[1].style.background = "black";
     li[1].children[0].style.color = "white";
 
-    li[0].style.background = "red";
+    li[0].style.backgroundColor = "red";
     li[0].children[0].style.color = "white";
+    li[0].children[0].style.backgroundColor = "red";
     document.getElementById('nav-list').style.display = 'none';
     document.getElementById('nav-list2').style.display = 'block';
 })
 var div = document.createElement('div');
 div.id='searchData';
 div.style.display='none';
-document.getElementById('nav-center').appendChild(div);
 document.addEventListener('mouseover',function(e){
-    if(e.target.parentNode.id=='nav-list'){
-       
-        div.style.display="block";
-        div.innerHTML=e.target.innerHTML;
+    if(e.target.className=='nav-list-tag'){
+       e.target.appendChild(div);
+    //    e.target.style.background = "white";
+    //    e.target.style.color = "red";
+        
+        div.innerHTML='';
+        var text = '';
+        for(var i in nav[e.target.innerText]){
+            text+='<p>'+i+'<span>></span></p><ul>';
+            var x=nav[e.target.innerText][i];
+            for(var j of nav[e.target.innerText][i]){
+                text+='<li>'+j+'</li>';
+            }
+            text+='</ul>';
+        }
+        div.innerHTML=text;
     }
 })
-document.addEventListener('mouseout',function(e){
-    if(e.target.parentNode.id=='nav-list'){
-       
-        div.style.display="none";
-        //div.innerHTML=e.target.innerHTML;
-    }
-})
-                                      
+
+  //左侧导航,制订搜索框  
+  var index = 1;    
+function indexImg(){
+      var pic = document.getElementById('img-nav-index');
+      var tag = document.getElementById('img-nav-imgtag');
+      if(index>4){
+          index=1
+      }
+      for(let i of tag.children){
+       i.style.background = "rgba(14, 13, 13, 0.534)";
+      }
+      tag.children[index-1].style.background = "white";
+      
+      pic.attributes.src.value = 'img/b1 ('+ index +').png';
+      index++;
+      
+
+      time=setTimeout(indexImg,2000);
+  }  
+  indexImg(); 
+  document.getElementById('img-nav').addEventListener('mousemove',function(e){
+      clearTimeout(time);
+      if(e.target.tagName=="LI"){
+        var pic = document.getElementById('img-nav-index');
+          index = e.target.tabIndex;
+          for(let i of e.target.parentNode.children){
+            i.style.background = "rgba(14, 13, 13, 0.534)";
+           }        
+          pic.attributes.src.value = 'img/b1 ('+ index +').png';
+      }
+
+  })  
+  document.getElementById('img-nav').addEventListener('mouseout',function(){
+    indexImg();
+
+})                
                                          
                        
                                 
